e7771be91cf55ca59dab7dda284d0363647a17b1
unknown <bradenlock83@gmail.com>
2022-10-15 18:41:55 -0500

    can send and download pdf via flask

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
-from flask import Flask, request, jsonify, render_template
-app = Flask(__name__)
+from flask import Flask, request, redirect, jsonify, render_template, send_from_directory, url_for
+import os
+from app.torch_utils import test
 
-@app.route('/')
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploaded_files'
+
+@app.route('/', methods=['GET', 'POST'])
 def home_page():
+    if request.method == "POST":
+        if request.files:
+            pdf = request.files["pdf"]
+            pdf.save(os.path.join(app.config['UPLOAD_FOLDER'], pdf.filename))
+            test()
+            return send_from_directory(app.config['UPLOAD_FOLDER'], pdf.filename, as_attachment=True)
     return render_template('home.html')
 
 @app.route('/predict', methods=['POST'])
