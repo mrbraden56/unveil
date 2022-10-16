f618d645d036e7f93634fac983956fcf365fac30
unknown <bradenlock83@gmail.com>
2022-10-15 23:07:53 -0500

    api accepts pdf and returns csv

--- app/main.py
+++ app/main.py
@@ -1,9 +1,33 @@
+import csv
+from lib2to3.pgen2 import token
 from flask import Flask, request, redirect, jsonify, render_template, send_from_directory, url_for
 import os
-from app.torch_utils import test
+from app.torch_utils import SummarizeParagraph
+from transformers import BertForSequenceClassification
+from transformers import BertTokenizer
+import requests
+
+def load_model():
+    model = BertForSequenceClassification.from_pretrained(
+        "bert-base-uncased",
+        num_labels = 2,      
+        output_attentions = False,
+        output_hidden_states = False,
+    )
+    return model
+
+def load_tokenizer():
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+    return tokenizer
+
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploaded_files'
+app.config['UPLOAD_FOLDER'] = 'uploaded_files/'
+app.config['DOWNLOAD_FOLDER'] = 'download_files/'
+model=load_model()
+model_trained = model.from_pretrained("saved_model")
+tokenizer=load_tokenizer()
+tokenizer_trained = tokenizer.from_pretrained("saved_model")
 
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
@@ -11,8 +35,16 @@ def home_page():
         if request.files:
             pdf = request.files["pdf"]
             pdf.save(os.path.join(app.config['UPLOAD_FOLDER'], pdf.filename))
-            test()
-            return send_from_directory(app.config['UPLOAD_FOLDER'], pdf.filename, as_attachment=True)
+            csv_name=pdf.filename.replace('pdf', 'csv')
+            print(pdf.filename)
+            print(csv_name)
+            summarize=SummarizeParagraph(pdf_name=pdf.filename, 
+                                         upload_folder=app.config['UPLOAD_FOLDER'], 
+                                         model=model, 
+                                         tokenizer=tokenizer)
+            risk_df=summarize.run()
+            risk_df.to_csv(app.config['DOWNLOAD_FOLDER']+csv_name)
+            return send_from_directory(app.config['DOWNLOAD_FOLDER'], csv_name, as_attachment=True)
     return render_template('home.html')
 
 @app.route('/predict', methods=['POST'])
--- app/torch_utils.py
+++ app/torch_utils.py
@@ -1,2 +1,75 @@
-def test():
-    print('in different file')
+from ast import List
+from configparser import ConfigParser
+from lib2to3.pgen2.tokenize import tokenize
+import os
+import PyPDF2
+from tokenizers import decoders, models, normalizers, pre_tokenizers, processors, trainers, Tokenizer
+import nltk
+nltk.download('punkt')
+from nltk import tokenize
+from nltk.tokenize import BlanklineTokenizer
+import torch
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+import pandas as pd
+
+class SummarizeParagraph:
+    def __init__(self, pdf_name: str, upload_folder: str, model, tokenizer) -> None:
+        self.pdf_name: str = pdf_name
+        self.upload_folder: str = upload_folder
+        self.model_trained = model.from_pretrained("saved_model")
+        self.tokenizer_trained = tokenizer.from_pretrained("saved_model")
+        self.peg_tokenizer=PegasusTokenizer.from_pretrained("google/pegasus-xsum")
+        self.peg_model=PegasusForConditionalGeneration.from_pretrained("google/pegasus-xsum")
+
+    def classify_paragraph(self, model, tokenizer, paragraph):
+        input_ids = []
+        attention_masks = []
+        encoded_dict=tokenizer.encode_plus(
+                                paragraph,
+                                add_special_tokens = True,
+                                max_length = 500,
+                                pad_to_max_length = True,
+                                return_attention_mask = True,
+                                return_tensors = 'pt',
+                        )
+        input_ids.append(encoded_dict['input_ids'])
+        attention_masks.append(encoded_dict['attention_mask'])
+        result = model(input_ids[0], 
+                            token_type_ids=None, 
+                            attention_mask=attention_masks[0],
+                            return_dict=True)
+        result=int(torch.argmax(result.logits))
+        if result==1: return True
+        else: return False
+
+    def summarize(self, paragraph):
+        tokens = self.peg_tokenizer(paragraph, truncation=True, padding="longest", return_tensors="pt")
+        output = self.peg_model.generate(**tokens)
+        summary=self.peg_tokenizer.decode(output[0])
+        return summary
+
+
+    def run(self):
+        pdfFileObj = open(self.upload_folder+self.pdf_name, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        number_of_pages=pdfReader.numPages
+        risk_dic={"Original": [], "Summarized": []}
+        for i in range(0,number_of_pages):
+            pageObj = pdfReader.getPage(i)
+            pdf_page=pageObj.extractText()
+            paragraphs = BlanklineTokenizer().tokenize(pdf_page)
+            for i, paragraph in enumerate(paragraphs):
+                result=self.classify_paragraph(self.model_trained, self.tokenizer_trained, paragraph)
+                print(i)
+                if result:
+                    summary=self.summarize(paragraph)
+                    risk_dic["Original"].append(paragraph)
+                    risk_dic["Summarized"].append(summary)
+                    print(f"Original:\n{paragraph}\nSummary:\n{summary}")
+                else:
+                    continue
+            if i==5:
+                break
+            print(f"Page: {i}")
+        risk_dic = pd.DataFrame.from_dict(risk_dic)
+        return risk_dic
